c392366e83e3895e9cdecbc2d2530860cb83c7cb
Nirob Hasan <thenirobhasan@gamil.com>
2020-03-11 00:35:57 +0600

    created authentication system

--- users/views.py
+++ users/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-# Create your views here.
-def index(request):
-    return render(request,'home/navbar.html')
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Your Account has been Created! You are now able to login {username}!')
+            return redirect('login')
+    else:        
+        form = UserRegisterForm()
+    return render(request,'users/register.html',{'form':form})
+    
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')    
