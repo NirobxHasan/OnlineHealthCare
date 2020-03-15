e3ef21af7be9546422fff9634f277e568ce0458d
Nirob Hasan <thenirobhasan@gamil.com>
2020-03-15 16:03:52 +0600

    created profile

--- users/apps.py
+++ users/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
-
+from __future__ import unicode_literals
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        import users.signals
